8ff7919b3e34be9601ab83228b9448ac0a532c1b
imranjeetsingh <ranjee970@gmail.com>
2019-03-23 23:30:46 +0530

    added comment delete method

--- DjangoBlog/src/comments/views.py
+++ DjangoBlog/src/comments/views.py
@@ -8,8 +8,38 @@ from django.contrib import messages
 from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 
+def comment_delete(request,id):
+    # obj = get_object_or_404(Comment,id=id)
+    try:
+        obj = Comment.objects.get(id=id)
+    except:
+        raise Http404
+    if request.user != obj.user:
+        response = HttpResponse("You don't have permission to do this.")
+        response.status_code = 403
+        return response
+
+    if request.method =="POST":
+        parent_obj_url = obj.content_object.get_absolute_url()
+        obj.delete()
+        messages.success(request,"Comment was deleted successfully")
+        return HttpResponseRedirect(parent_obj_url)
+    context = {
+        "object":obj,
+    }
+    return render(request,"confirm_delete.html",context)
+
 def comment_thread(request,id):
-    obj = get_object_or_404(Comment, id=id)
+    # obj = get_object_or_404(Comment, id=id)
+    try:
+        obj = Comment.objects.get(id=id)
+    except:
+        raise Http404
+
+    if not obj.is_parent:
+        obj = obj.parent
+    # print(obj)
+    
     content_object = obj.content_object
     content_id = obj.object_id
     initial_data = {
